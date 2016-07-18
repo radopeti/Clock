8c6f7d8c2c7b422a9cdcb4db3408b54e64e7ddf6
Peter Rado <rado.peti@gmail.com>
2016-07-18 13:11:05 +0200

    Create repository
    This project is going to represent a simple clock with geometry
    elements.

---
+++ android/src/com/blogspot/tinydevbugs/clock/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.blogspot.tinydevbugs.clock.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.blogspot.tinydevbugs.clock.MyGdxClock;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxClock(), config);
+	}
+}
---
+++ desktop/src/com/blogspot/tinydevbugs/clock/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.blogspot.tinydevbugs.clock.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.blogspot.tinydevbugs.clock.MyGdxClock;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxClock(), config);
+	}
+}
