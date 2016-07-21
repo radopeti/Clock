92aa3fa39767c4dc6972a1dec879850bcf80e46b
Peter Rado <rado.peti@gmail.com>
2016-07-21 10:26:59 +0200

    Separate constants and add comments

---
+++ core/src/com/blogspot/tinydevbugs/clock/Constants.java
@@ -0,0 +1,20 @@
+package com.blogspot.tinydevbugs.clock;
+
+import com.badlogic.gdx.graphics.Color;
+
+/**
+ * Created by Peter Rado on 2016. 07. 21..
+ */
+
+
+public class Constants {
+    public static final int HOUR_HAND_SIZE = 70;
+    public static final int MINUTE_HAND_SIZE = 110;
+    public static final int SECOND_HAND_SIZE = 100;
+    public static final int CIRCLE_RADIUS = 120;
+    public static final double HOUR_HAND_ANGLE = 30;
+    public static final double MINUTE_HAND_ANGLE = 6;
+    public static final double SECOND_HAND_ANGLE = 6;
+    public static final Color HAND_COLOR = Color.WHITE;
+    public static final Color SECOND_HAND_COLOR = Color.RED;
+}
--- core/src/com/blogspot/tinydevbugs/clock/VisualClock.java
+++ core/src/com/blogspot/tinydevbugs/clock/VisualClock.java
@@ -1,80 +1,106 @@
 package com.blogspot.tinydevbugs.clock;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
-
+import static com.blogspot.tinydevbugs.clock.Constants.*;
 import java.time.LocalTime;
 
 /**
- * Created by hátén on 2016. 07. 18..
+ * Created by Peter Rado on 2016. 07. 18..
+ * Represent a visual clock build by simple shapes
  */
 public class VisualClock {
-
-    private static final int HOUR_HAND_SIZE = 70;
-    private static final int MINUTE_HAND_SIZE = 110;
-    private static final int SECOND_HAND_SIZE = 100;
-    private static final double HOUR_HAND_ANGLE = 30;
-    private static final double MINUTE_HAND_ANGLE = 6;
-    private static final double SECOND_HAND_ANGLE = 6;
-    private static final Color HAND_COLOR = Color.WHITE;
-    private static final Color SECOND_HAND_COLOR = Color.RED;
+    //the middle point of the clock
     private Vector2 midPoint;
+    //the second coordinates of the lines which represent the hands of the clock
     private float hourX;
     private float hourY;
     private float minuteX;
     private float minuteY;
     private float secondX;
     private float secondY;
+    //the class uses java.time.LocalTime the determine the current system time
     private LocalTime localTimeNow;
 
+    /**
+     * Constructor with test middle point coordinates
+     */
     public VisualClock(){
         midPoint = new Vector2(150, 150);
-
     }
 
+    /**
+     * Update the current position of the hands based on the current system time
+     */
     public void update(){
         localTimeNow = LocalTime.now();
         int currentHour = localTimeNow.getHour();
-
         int hour = currentHour > 12 ? currentHour - 12 : currentHour;
         int minute = localTimeNow.getMinute();
         int second = localTimeNow.getSecond();
-
-
-        hourX = (float) calculateHandEndPointX(HOUR_HAND_SIZE, HOUR_HAND_ANGLE, hour, minute);
-        hourY = (float) calculateHandEndPointY(HOUR_HAND_SIZE, HOUR_HAND_ANGLE, hour, minute);
+        hourX = (float) calculateHandEndPointX(hour, minute);
+        hourY = (float) calculateHandEndPointY(hour, minute);
         minuteX = (float) calculateHandEndPointX(MINUTE_HAND_SIZE, MINUTE_HAND_ANGLE, minute);
         minuteY = (float) calculateHandEndPointY(MINUTE_HAND_SIZE, MINUTE_HAND_ANGLE, minute);
         secondX = (float) calculateHandEndPointX(SECOND_HAND_SIZE, SECOND_HAND_ANGLE, second);
         secondY = (float) calculateHandEndPointY(SECOND_HAND_SIZE, SECOND_HAND_ANGLE, second);
     }
 
+    /**
+     * Draw and set the color of the visual components of the clock
+     * @param renderer ShapeRenderer
+     */
     public void render(ShapeRenderer renderer){
         renderer.setColor(HAND_COLOR);
-        renderer.circle(midPoint.x, midPoint.y, 120);
+        renderer.circle(midPoint.x, midPoint.y, CIRCLE_RADIUS);
         renderer.line(midPoint.x, midPoint.y, hourX, hourY);
         renderer.line(midPoint.x, midPoint.y, minuteX, minuteY);
         renderer.setColor(SECOND_HAND_COLOR);
         renderer.line(midPoint.x, midPoint.y, secondX, secondY);
     }
 
+    /**
+     * Calculate the second x coordinate of the hand
+     * @param handSize the size of the hand (line) we want to see on the screen
+     * @param angle one unit of a hand step in degrees
+     * @param clockParam the current time in hour, minute or second depends on which hand's
+     *                   coordinate we want to calculate
+     * @return the second x coordinate of the hand
+     */
     private double calculateHandEndPointX(int handSize, double angle, int clockParam){
         return midPoint.x + handSize * Math.sin(MathUtils.degreesToRadians * (clockParam * angle));
     }
 
+    /**
+     * Calculate the second y coordinate of the hand
+     * @param handSize the size of the hand (line) we want to see on the screen
+     * @param angle one unit of a hand step in degrees
+     * @param clockParam the current time in hour, minute or second depends on which hand's
+     *                   coordinate we want to calculate
+     * @return the second y coordinate of the hand
+     */
     private double calculateHandEndPointY(int handSize, double angle, int clockParam){
         return midPoint.y + handSize * Math.cos(MathUtils.degreesToRadians * (clockParam * angle));
     }
 
-    private double calculateHandEndPointX(int handSize, double angle, int clockParam, int minute){
-        Gdx.app.log("Clock", "angle " + (clockParam * angle + (angle / 60 * minute)));
-        return midPoint.x + handSize * Math.sin(MathUtils.degreesToRadians * (clockParam * angle + (angle / 60 * minute)));
+    /**
+     * Calculate the hour hand's second x coordinate
+     * @param clockParam the current hour
+     * @param minute the current minute
+     * @return the second x coordinate of the hour hand
+     */
+    private double calculateHandEndPointX(int clockParam, int minute){
+        return midPoint.x + HOUR_HAND_SIZE * Math.sin(MathUtils.degreesToRadians * (clockParam * HOUR_HAND_ANGLE + (HOUR_HAND_ANGLE / 60 * minute)));
     }
 
-    private double calculateHandEndPointY(int handSize, double angle, int clockParam, int minute){
-        return midPoint.y + handSize * Math.cos(MathUtils.degreesToRadians * (clockParam * angle + (angle / 60 * minute)));
+    /**
+     * Calculate the hour hand's second y coordinate
+     * @param clockParam the current hour
+     * @param minute the current minute
+     * @return the second y coordinate of the hour hand
+     */
+    private double calculateHandEndPointY(int clockParam, int minute){
+        return midPoint.y + HOUR_HAND_SIZE * Math.cos(MathUtils.degreesToRadians * (clockParam * HOUR_HAND_ANGLE + (HOUR_HAND_ANGLE / 60 * minute)));
     }
 }
